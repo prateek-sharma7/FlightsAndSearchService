180374496f9bed9b18a10a3f24cd1f1c6e4436c3
Prateek <sharmaprateek499@gmail.com>
2023-10-22 06:28:49 +0530

    associated city and airport

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ const bodyParser = require("body-parser");
 const { PORT } = require("./config/serverConfig");
 const ApiRoutes = require("./router/index");
 
+const db = require("./models/index");
+/* const { City, Airport } = require("./models/index"); */
+
 const setupAndStartServer = async () => {
   // create the express object
   const app = express();
@@ -13,8 +16,9 @@ const setupAndStartServer = async () => {
 
   app.use("/api", ApiRoutes);
 
-  app.listen(PORT, () => {
+  app.listen(PORT, async () => {
     console.log(`Server started at PORT: ${PORT}`);
+    db.sequelize.sync({ alter: true });
   });
 };
 
